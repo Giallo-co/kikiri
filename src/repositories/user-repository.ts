7e7bc96324419a61f024872f01f4bbc8a0bf94f5
PhilipFalla <philipfalla@ufm.edu>
2026-02-05 09:09:56 -0600

    Steering away from Promise Hell

---
+++ src/repositories/user-repository.ts
@@ -0,0 +1,24 @@
+import type { User } from "../models/user.js"
+
+export class UserRepository {
+    callToExternalServiceAsync = async () : Promise<string> => {
+        
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                resolve("External call success.");
+            }, 200)
+        })
+    }
+
+    saveUserAsync = async (user : User) : Promise<void> => {
+        
+        await this.callToExternalServiceAsync();
+
+    }
+
+    saveRelationshipAsync = async (user : User) : Promise<void> => {
+        
+        await this.callToExternalServiceAsync();
+
+    }
+}
--- src/services/user-service.ts
+++ src/services/user-service.ts
@@ -1,12 +1,16 @@
 import type { User } from '../models/user.js'
 import { UserErrors } from '../errors/errors.js';
+import type { UserRepository } from '../repositories/user-repository.js';
 
 
 export class UserService {
     private PasswordLength : number = 8;
 
+    constructor(private readonly userRepository : UserRepository) {
+        console.log('Initilized user Repository')
+    }
     
-    registerUser( userToRegister: User ): User {
+    async registerUserAsync( userToRegister: User ): Promise<User> {
         console.log("Starting to register user!")
         const password = userToRegister.password;
 
@@ -15,19 +19,19 @@ export class UserService {
             throw UserErrors.InvalidPassword;
         }
 
-        if (password.length < this.PasswordLength) {
-            console.log("Password is too short!");
-            throw new Error("Password length");
+        let user : User = {
+            email: userToRegister.email,
+            username: userToRegister.username,
+            password: "encrypted",
+            id: userToRegister.id,
+            role: 0,
+            friends: []
         }
 
-        return {
-            email : userToRegister.email,
-            username : userToRegister.username,
-            password : "encrypted",
-            id : userToRegister.id,
-            role : 0,
-            friends : []
-        }
+        let promise1 = this.userRepository.saveUserAsync(user);
+        let promise2 = this.userRepository.saveRelationshipAsync(user);
+        await Promise.all([promise1, promise2]); //concurrent promises
+        return user;
     }
     
 
