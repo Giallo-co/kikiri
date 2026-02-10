cc755605123eaa81c1d210554aa1c7c122e83779
Juan Ochaita <juanom@ufm.edu>
2026-02-10 08:58:14 -0600

    FUNCIONA

--- src/test/user-test.ts
+++ src/test/user-test.ts
@@ -1,22 +1,22 @@
-import { UserRepository } from "../repositories/user-repository";
-import { UserService } from "../services/user-service";
-import type { User } from "../models/user";
+import { UserRepository } from '../repositories/user-repository.js';
+import { UserService } from '../services/user-service.js';
+import type { User } from '../models/user.js';
 
 describe('UserService', () => {
-    it('Should return a new User', async () => {
-        const userRepository = new UserRepository();
-        const userService = new UserService(userRepository);
+  it('should return a new User', async () => {
+    const userRepository = new UserRepository();
+    const userService = new UserService(userRepository);
 
-        let user : User = {
-            email: "philipfallag@gmail.com",
-            username: "Philip",
-            password: "12345678",
-            id: 1,
-            role: 2,
-            friends: []
-        }
-        const result = await userService.registerUserAsync(user);
+    let user : User = {
+        email : "joseguzman@gmail.com",
+        password : "encrypted",
+        username : "Jose",
+        id : 100,
+        role : 0,
+        friends : []
+    };
+    const result = await userService.registerUserAsync(user);
 
-        expect(result).toBe(user);
-    })
-})
+    expect(result).toMatchObject(user);
+  });
+});
