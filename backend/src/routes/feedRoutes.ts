5d093fdf83261ac07fd9052c14af018a925a3073
PhilipFalla <philipfalla@ufm.edu>
2026-02-25 22:35:33 -0600

    updated feedRouter.ts

--- backend/src/routes/feedRoutes.ts
+++ backend/src/routes/feedRoutes.ts
@@ -2,13 +2,15 @@ import { Router } from 'express';
 import { FeedController } from '../controllers/feedController';
 import { FeedService } from '../services/feedService';
 import { PostRepository } from '../repositories/postRepository';
+import { UserRepository } from '../repositories/userRepository';
 
 const router = Router();
 
 const postRepository = new PostRepository();
-const feedService = new FeedService(postRepository);
+const userRepository = new UserRepository();
+const feedService = new FeedService(postRepository, userRepository);
 const feedController = new FeedController(feedService);
 
-router.get("v1/feed", (req, res, next) => feedController.getByUser(req, res, next));
+router.get("/v1/feed/:userId", (req, res, next) => feedController.getFeed(req, res, next));
 
 export default router;
