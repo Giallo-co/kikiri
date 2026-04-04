84e29d96c01c97fc1baaf1ed32400e1898133279
jonatan <josue.aguilar@ufm.edu>
2026-04-04 15:44:11 -0600

    auth middleware

---
+++ backend/src/middlewares/authMiddleware.ts
@@ -0,0 +1,23 @@
+import { Request, Response, NextFunction } from 'express';
+import jwt from 'jsonwebtoken';
+
+export const authenticateToken = (req: Request, res: Response, next: NextFunction) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader || !authHeader.startsWith('Bearer ')) {
+    return res.status(401).json({ error: 'No token provided' });
+  }
+
+  const token = authHeader.split(' ')[1] as string;
+
+  try {
+    const jwtSecretKey = process.env.JWT_SECRET_KEY as string;
+    const decoded = jwt.verify(token, jwtSecretKey);
+    
+    (req as any).user = decoded; 
+    
+    next();
+  } catch (error) {
+    return res.status(403).json({ error: 'Invalid or expired token' });
+  }
+};
