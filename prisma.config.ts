bf636d646f5ddb06278a0b4b2489e769603ed038
jonatan <josue.aguilar@ufm.edu>
2026-04-20 23:42:26 -0600

    feat: prima dummy url

--- prisma.config.ts
+++ prisma.config.ts
@@ -3,15 +3,17 @@ import { defineConfig } from "@prisma/config";
 import mariadb from "mariadb";
 import { PrismaMariaDb } from "@prisma/adapter-mariadb";
 
+const databaseUrl = process.env.DATABASE_URL || "mysql://dummy:password@localhost:3306/dummy";
+
 export default defineConfig({
   schema: "prisma/schema.prisma",
   migrations: {
     path: "prisma/migrations",
   },
   datasource: {
-    url: process.env.DATABASE_URL!,
+    url: databaseUrl,
     adapter: () => {
-      const pool = mariadb.createPool(process.env.DATABASE_URL!);
+      const pool = mariadb.createPool(databaseUrl);
       return new PrismaMariaDb(pool);
     },
   },
