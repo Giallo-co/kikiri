67e42de6ff5bed372ee3f4ada924ce1a27295163
HugoGALAGA <hugogalina@ufm.edu>
2026-04-21 00:01:47 -0600

    Update searchRoutes.ts

--- src/routes/searchRoutes.ts
+++ src/routes/searchRoutes.ts
@@ -8,6 +8,6 @@ router.get('/v1/search/users', (req, res, next) => searchController.searchUsers(
 
 router.get('/v1/search/users/:id', (req, res, next) => searchController.getUser(req, res, next));
 
-router.get('/posts', (req, res, next) => controller.searchPosts(req, res, next));
+router.get('/posts', (req, res, next) => searchController.searchPosts(req, res, next));
 
 export default router;
